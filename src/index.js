a5a102f11bb9b9c22ed545c62361dff3c0747882
anurag-asu <adhasma1@asu.edu>
2021-11-28 18:23:46 -0800

    added a route for getting hash from Pinata

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const multer = require("multer")
+
 const router = express.Router();
+const upload = multer()
 
 const app = express()
-const port = 3000;
+
+const port = 3001;
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +17,7 @@ const { publishMessage } = require('./publisher')
 const { get_ipfs_hash } = require('./pinata')
 
 router.post('/publish', async(req, res)=>{
-    console.log(req.body)
+
     let { image_url = '', image_hash = '' } = req.body;
 
     const payload = {
@@ -28,17 +32,19 @@ router.post('/publish', async(req, res)=>{
   })
 })
 
-router.post('/pinata', async(req, res)=>{
+router.post('/pinata', upload.single('file'), async (req, res) => {
   
-  let { imageData } = req.body;
+  const pinata_data = await get_ipfs_hash(req.file);
   
-  const resp = await get_ipfs_hash(imageData);
-
-  console.log(resp);
-
-  res.status(200).send({
-    resp
-  });
+  if(pinata_data){
+    res.status(200).send({
+      pinata_data
+    });
+  }  else {
+    res.status(500).send({
+      'error': "oops! something went wrong"
+    });
+  }
 })
 
 app.use("/", router);
--- src/pinata.js
+++ src/pinata.js
@@ -1,27 +1,38 @@
 const request = require('request-promise');
+const fs = require('fs');
+const axios = require('axios')
+const formData = require('form-data')
+const { Readable } = require('stream');
 
-const get_ipfs_hash = async (imageData, pinataApiKey, pinataSecretApiKey) => {
 
+const get_ipfs_hash = async (file) => {
     try {
 
-        const resp = await request({
-            headers: {
-                'Content-Type': `multipart/form-data; boundary= ${imageData._boundary}`,
-                'pinata_api_key': pinataApiKey,
-                'pinata_secret_api_key': pinataSecretApiKey
-            },
-            uri: 'https://api.pinata.cloud/pinning/pinJSONToIPFS',
-            body: imageData,
-            method: 'POST'
-        });
-        
-        return resp;
+        const stream = Readable.from(file.buffer)
+        stream.path = file.originalname
+
+        let data = new formData()
+        data.append('file', stream);
+
+        const pinata_resp = await axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS',
+            data,
+            {
+                maxContentLength: 'Infinity', //this is needed to prevent axios from erroring out with large files
+                headers: {
+                    'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+                    'pinata_api_key': '4663dc103891764f7960',
+                    'pinata_secret_api_key': '1a4f0f57e41a5f7c644fb84f740ca77cdc6e7cca55fb050d9c31df58b0374a62'
+                }
+            }
+        )
+
+        return pinata_resp.data
 
     } catch(e) {
        console.log('get_ipfs_hash error ', e)
+       return null
     }
   
-    
 }
 
 module.exports = { get_ipfs_hash };
